34717154b6653a464004c3ded3a1fcfcb88d1a84
Muhammad Aditia Firmansyah <aditia.pikarin@gmail.com>
2021-12-11 18:01:08 +0700

    preserve scroll position

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,9 +1,22 @@
 <script setup>
+import { Link } from '@inertiajs/inertia-vue3'
 import AppNav from '../Shared/AppNav'
+
+const props = defineProps({
+    time: String,
+})
 </script>
 
 <template>
     <h1 class="text-4xl font-bold">Users</h1>
 
     <AppNav />
+
+    <div class="mt-[800px]">
+        <p>The current time is {{ props.time }}</p>
+
+        <Link href="/users" class="text-blue-500 hover:underline" preserve-scroll>
+            Refresh
+        </Link>
+    </div>
 </template>
